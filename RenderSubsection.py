c24b972923caf1018864707385b05b6a88383db0
Mason <mason.hoppe@gmail.com>
2020-11-30 14:02:28 -0600

    added quick playblast

--- RenderSubsection.py
+++ RenderSubsection.py
@@ -15,12 +15,14 @@ bl_info = {
 def register():
 	bpy.utils.register_class(CustomPropertyGroup)
 	bpy.utils.register_class(RenderSection)
+	bpy.utils.register_class(PlayblastSection)
 	bpy.utils.register_class(RenderSectionPanel)
 	bpy.types.Scene.custom_props = bpy.props.PointerProperty(type=CustomPropertyGroup)
 
 def unregister():
 	del bpy.types.Scene.custom_props
 	bpy.utils.unregister_class(RenderSectionPanel)
+	bpy.utils.unregister_class(PlayblastSection)
 	bpy.utils.unregister_class(RenderSection)
 	bpy.utils.unregister_class(CustomPropertyGroup)
 	
@@ -33,10 +35,10 @@ class CustomPropertyGroup(bpy.types.PropertyGroup):
 	stop_slider: bpy.props.IntProperty(name='Stop', soft_min=0, soft_max=100)
 	
 
-# the actual work
 class RenderSection(bpy.types.Operator):
 	bl_idname = "custom.render_section"
 	bl_label = "Render Section"
+	bl_description = "Render the timeline with above limits"
 	bl_options = {'REGISTER'}
 	def execute(self, context):
 		S = bpy.context.scene
@@ -51,12 +53,39 @@ class RenderSection(bpy.types.Operator):
 
 		# render
 		bpy.ops.render.render(animation=True)
+		#bpy.ops.render.opengl(animation=True)
 
 		#restore old start / end
 		S.frame_start = old_start
 		S.frame_end = old_end
+		self.report({'INFO'}, "Render Finished")
 		return {'FINISHED'}
 
+class PlayblastSection(bpy.types.Operator):
+	bl_idname = "custom.playblast_section"
+	bl_label = "Playblast Section"
+	bl_description = "Playblast the timeline with above limits"
+	bl_options = {'REGISTER'}
+	def execute(self, context):
+		S = bpy.context.scene
+
+		# Save old start / end
+		old_start = S.frame_start
+		old_end = S.frame_end
+
+		# set new start end
+		S.frame_start = S.custom_props.start_slider
+		S.frame_end = S.custom_props.stop_slider
+
+		# render
+		#bpy.ops.render.render(animation=True)
+		bpy.ops.render.opengl(animation=True)
+
+		#restore old start / end
+		S.frame_start = old_start
+		S.frame_end = old_end
+		self.report({'INFO'}, "Render Finished")
+		return {'FINISHED'}
 
 class RenderSectionPanel(bpy.types.Panel):
 	bl_idname = "OBJECT_PT_render_section"
@@ -66,11 +95,15 @@ class RenderSectionPanel(bpy.types.Panel):
 	bl_context = "objectmode"
 
 	def draw(self, context):
-		self.layout.operator('custom.render_section', text="Render")
 		self.layout.label(text="Render Limits:")
+		
 		subrow = self.layout.row(align=True)
 		subrow.prop(context.scene.custom_props, 'start_slider')
 		subrow.prop(context.scene.custom_props, 'stop_slider')
+		
+		renderRow = self.layout.row(align=False)
+		renderRow.operator('custom.render_section', text="Render")
+		renderRow.operator('custom.playblast_section', text="Playblast")
 		
 if __name__ == "__main__":
 	register()
